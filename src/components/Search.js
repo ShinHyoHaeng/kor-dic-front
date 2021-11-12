357db361d454c9dcd19fc358a617ee25202c35e3
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-11-12 12:44:26 +0900

    add select/input event

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,36 +2,49 @@ import React, { useRef, useState } from 'react'
 
 function Search() {
 
-    const [query, setQuery] = useState(""); // value=""와 동일
+    const [Query, setQuery] = useState(""); // value=""와 동일
     const queryRef = useRef();
+    const [Selected, setSelected] = useState("none");
 
-    const Search = () => {
-        const searchQuery = queryRef.current.value; // 현재 검색한 단어 
-        setQuery(searchQuery); 
-        console.log(searchQuery);
+
+    //select
+    const Options = [
+        {value:"none", name:"전체"},
+        {value:"word", name:"단어"},
+        {value:"mean", name:"의미"},
+        {value:"wordclass", name:"품사"}
+    ]
+    const SelectBox = (props) => {
+        const handleSelect = (e) => { 
+            setSelected(e.target.value);
+            console.log(e.target.value);
+        }
+        return(
+            <select name="select" className="form-control" id="select" onChange={handleSelect}>
+                {props.options.map((option) => (
+                    <option key={option.value} value={option.value} defaultValue={props.defaultValue === option.value}>{option.name}</option>
+                ))}
+            </select>
+        )
     }
 
-    const onKeyPressStart = (e) => { // 엔터키 이벤트
-        if(e.key === 'Enter'){
-            Search();
-        }
-    };
+    // input
+    const handleInput = (e) => {
+        setQuery(e.target.value);
+        console.log(Query);
+    }
 
     return (
         <div className="searchInner">
             <div className="col-3 selectArea">
-                <select name="select" className="form-control" id="select">
-                    <option value="none" selected>전체</option>
-                    <option id="word" value="word">단어</option>
-                    <option id="mean" value="mean">의미</option>
-                    <option id="wordclass" value="wordclass">품사</option>
-                </select>
+                {/* Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>. */}
+                <SelectBox options={Options} defaultValue="none"></SelectBox>
             </div>
             <div className="col-6 inputArea">
-                <input type="text" placeholder="단어를 입력해주세요" name="" id="search" className="form-control" ref={queryRef} onKeyPress={onKeyPressStart} autoFocus/>
+                <input type="text" placeholder="단어를 입력해주세요" id="search" className="form-control"  onChange={(e)=>handleInput(e)} autoFocus/>
             </div>
             <div className="col-3 buttonArea">
-                <input type="submit" className="form-control" name="" id="submit" value="검색" onClick={Search}/>
+                <input type="submit" className="form-control" id="submit" value="검색"/>
             </div>
         </div>   
     )
