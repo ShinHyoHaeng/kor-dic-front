c710ab8b600e8a2894decfe9cd2c461dcc0788d6
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-11-12 11:54:36 +0900

    (참고용) 검색 관련 이벤트 추가

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react'
-export class Search extends Component {
+import React, { useRef, useState } from 'react'
 
+function Search() {
 
-    render() {
-        return (
-            <div className="searchInner">
-                <div className="col-3 selectArea">
-                    <select name="select" className="form-control" id="select">
-                        <option value="none" selected>전체</option>
-                        <option id="word" value="word">단어</option>
-                        <option id="mean" value="mean">의미</option>
-                        <option id="wordclass" value="wordclass">품사</option>
-                    </select>
-                </div>
-                <div className="col-6 inputArea">
-                    <input type="text" placeholder="단어를 입력해주세요" name="" id="search" value="" className="form-control" />
-                </div>
-                <div className="col-3 buttonArea">
-                    <input type="submit" className="form-control" name="" id="submit" value="검색" />
-                </div>
-            </div>   
-        )
+    const [query, setQuery] = useState(""); // value=""와 동일
+    const queryRef = useRef();
+
+    const Search = () => {
+        const searchQuery = queryRef.current.value; // 현재 검색한 단어 
+        setQuery(searchQuery); 
+        console.log(searchQuery);
     }
+
+    const onKeyPressStart = (e) => { // 엔터키 이벤트
+        if(e.key === 'Enter'){
+            Search();
+        }
+    };
+
+    return (
+        <div className="searchInner">
+            <div className="col-3 selectArea">
+                <select name="select" className="form-control" id="select">
+                    <option value="none" selected>전체</option>
+                    <option id="word" value="word">단어</option>
+                    <option id="mean" value="mean">의미</option>
+                    <option id="wordclass" value="wordclass">품사</option>
+                </select>
+            </div>
+            <div className="col-6 inputArea">
+                <input type="text" placeholder="단어를 입력해주세요" name="" id="search" className="form-control" ref={queryRef} onKeyPress={onKeyPressStart} autoFocus/>
+            </div>
+            <div className="col-3 buttonArea">
+                <input type="submit" className="form-control" name="" id="submit" value="검색" onClick={Search}/>
+            </div>
+        </div>   
+    )
 }
 
 export default Search
