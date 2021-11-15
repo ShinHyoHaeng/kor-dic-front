7c70fd5db1f295340d0634ead60f64c15303ef7f
wndltkr <92345437+wndltkr@users.noreply.github.com>
2021-11-15 15:41:51 +0900

    validation
    val

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,45 @@
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react/cjs/react.development';
+import SearchBar from './SearchBar';
+import datas from './Keyword';
 
-function Search(props) {
-
+const Search = (props) => {
+    // 검색어 자동완성에 쓸 데이터
+    const [data, setData] = useState(datas);
+    // 입력창에 입력한 텍스트 
     const [Query, setQuery] = useState(""); // value=""와 동일
+    // 검색어 자동완성 텍스트
+    const [results, setResult] = useState([]);
     const queryRef = useRef();
     const [Selected, setSelected] = useState("none");
-    const [isException, setIsException] = useState(false)
-    const [exceptionType, setExeptionType] = useState('');
+    const [exceptionType, setexceptionType] = useState('');
+    const [isException, setException] = useState(false);
+  
+    // 필드를 업데이트 
+    const updateField = (field, value, update = true) => {
+        // onSearch에 입력창에 입력된 텍스트을 넘긴다.
+        if (update) onSearch(value);
+        // 필드가 keyword면 keyword값 변경 / results면 results값 변경
+        if (field === 'keyword') { setQuery(value); }
+        if (field === 'results') { setResult(value); }
+    }
+  
+    // 입력된 텍스트로 data 배열에서 찾아 매칭되는 결과들을 저장 
+    const onSearch = text => {
+        var results = data.filter(item => true === matchName(item.word, text));
+        setResult({ results });
+    };
+
+    // 검색해야할 문자열을 키워드와 비교하여 매칭이 되는지 체크 
+    // word는 데이터베이스에 있는 단어 / keyword는 입력한 단어
+    const matchName = (word, keyword) => {
+        var keyLen = keyword.length;
+        // 데이터베이스에 있는 단어를 입력한 텍스트의 개수만큼 자른다.
+        word = word.toLowerCase().substring(0, keyLen);
+        // 입력된 텍스트가 없으면 false
+        if (keyword === "") return false;
+        // 데이터베이스에 있는 단어 앞부분과 입력된 텍스트를 비교해서 일치하면 true 아니면 false
+        return word === keyword.toString().toLowerCase();
+    };
 
     //select
     const Options = [
@@ -29,40 +62,31 @@ function Search(props) {
         )
     }
 
-    // input event
-    // const onFiliter = (e) => {
-    //     handleInput(e)
-    // }
-
-    // button event
-    const onFilter = (e) => {
-        setQuery(e.target.value)
-    }
-
     const onClick = () => {
         props.handleInput(Query)
     }
 
     // enter키 이벤트 추가
     const onKeyEvent = (e) => {
-        if(e.key === 'Enter'){onClick()}
+        if(e.key === 'Enter'){
+            onClick()
+        } else if (e.keyCode === 40) {
+            console.log('1')
+            queryRef.current.focus()
+        } else if (e.keyCode === 38) {
+            console.log('2')
+            queryRef.current.focus()
+        }
     }
 
     return (
         <div className="searchInner">
             <div className="col-3 selectArea">
+                {/* Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>. */}
                 <SelectBox options={Options} defaultValue="none"></SelectBox>
             </div>
-            <div className="col-6 inputArea">
-                <input type="text" 
-                    placeholder="단어를 입력해주세요" 
-                    id="search" 
-                    className="form-control" 
-                    onChange={(e)=>onFilter(e)} 
-                    onKeyPress={onKeyEvent}
-                    autoFocus
-                    />
-            </div>
+                <SearchBar keyword={Query} results={results} updateField={updateField} onKeyPress={e => onKeyEvent(e)} textInput={queryRef}></SearchBar>
+                {/* <SearchBar handleChange={(e)=>onFilter(e)} onKeyPress={onKeyEvent}></SearchBar> */}
             <div className="col-3 buttonArea">
                 <input 
                     type="submit" 
@@ -73,7 +97,7 @@ function Search(props) {
                 />
             </div>
         </div>   
-    )
+    );
 }
 
-export default Search
+export default Search;
