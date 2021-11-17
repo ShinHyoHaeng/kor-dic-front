ac034bbe9028e6548946cf8a33dc80b8fe5e0367
ShinHyoHaeng <hyohaeng.shin@gmail.com>
2021-11-17 14:56:44 +0900

    icon class명 추가

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Result({seq, keyword, link, hanja, word_class, meaning}) {
     return (
         <div className="item move-up">
-            <div className="word">{keyword}<sup>{seq}</sup><p className="hanja">{hanja}</p> <a href={link}>상세 정보 <ArrowForward/></a></div>
+            <div className="word">{keyword}<sup>{seq}</sup><p className="hanja">{hanja}</p> <a href={link}>상세 정보 <ArrowForward className="icon"/></a></div>
             <p className="description"><span>{word_class}</span> {meaning}</p>
         </div>
     )
